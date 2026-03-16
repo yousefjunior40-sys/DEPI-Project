9b67bc78764560a5f53ca76629eb34a9b9c4724a
youanes <yanesrefat@gmail.com>
2026-03-16 03:16:06 +0200

    Add Truck Grid feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import './App.css';
-import Header from "./components/header";
-import Footer from "./components/footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import TruckGrid from './components/TruckGrid';
+import trucks from './components/trucks';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
-      <>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Header />
-        <Routes></Routes>
-        <Footer />
-      </>
-    </BrowserRouter>
-  )
-}
 
-export default App
+        <main style={{ flex: 1, background: '#EBEAE0', padding: '2rem' }}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TruckGrid
+                  trucks={trucks}
+                  isLoading={false}
+                  title="Featured Food Trucks"
+                  onSeeAll={() => console.log('See all clicked')}
+                  onCardClick={(truck) => console.log('Clicked:', truck.name)}
+                />
+              }
+            />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
