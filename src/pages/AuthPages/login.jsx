ba84f41c675ee4f3ce33c92956028dca94a752c5
Muhammed Ramadan <muhammed.bussiness9@gmail.com>
2026-03-18 23:12:47 +0200

    New Updates for desgin

--- src/pages/AuthPages/login.jsx
+++ src/pages/AuthPages/login.jsx
@@ -1,26 +1,64 @@
     import { Link } from "react-router-dom";
-    import loginImg from "../../assets/logo-3.png"
+    import loginImg from "../../assets/logo-2.png"
     import "./login.css";
 
     function loginPage(){
         return(
         <div className="loginMain">
             <div className="login">
-                <form>
-                    <h1>Login</h1>
-                    <input type="text" name="username" placeholder="Username" required/>
-                    <input type="password" name="password" placeholder="Password" required/>
-                    <div className="pt2">
-                        <label>
-                            <input type="checkbox"/>
-                            Remember Me
-                        </label>
-                        <Link to="">Forget Password?</Link>
-                        <Link to="/register">Register?</Link>
-                    </div>
-                    <button className="loginBtn" type="submit">Sign In</button>
-                </form>
+
                 <img src={loginImg} alt="Truckspot"/>
+                <h1>Welcome back</h1>
+                <p>Sign in to your TruckSpot account</p>
+
+                <form>
+                    <label>Email</label>
+                    <div className="email">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" 
+                        viewBox="0 0 24 24" fill="none" stroke="#464343b2" stroke-width="2" 
+                        stroke-linecap="round" stroke-linejoin="round" 
+                        class="lucide lucide-mail absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground">
+                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
+                        <input type="text" name="username" placeholder="you@example.com" required/>
+                    </div> 
+
+                    <label>Password</label>
+                    <div className="pass">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 24 24" fill="none" stroke="#464343b2" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" 
+                        class="lucide lucide-lock absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground">
+                        <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
+                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
+                        <input type="password" name="password" placeholder="••••••••" required/>
+                    </div>
+
+                    <button className="loginBtn" type="submit">Sign In</button>
+                    <div className="d3">
+                        <hr />
+                        <p>or continue with</p>
+                        <hr />
+                    </div>
+
+                    <div className="pt2">
+
+                        <button className="googleBtn" type="button">
+                            <svg class="h-4 w-4" viewBox="0 0 24 24">
+                            <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 01-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z" fill="#4285F4"></path>
+                            <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"></path>
+                            <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"></path>
+                            <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"></path>
+                            </svg>Google</button>
+
+                        <button className="appleBtn" type="button">
+                            <svg class="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"></path>
+                            </svg>Apple</button>
+                    </div>
+                    <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+                    <p><Link to="/resetPass">Forget Password</Link></p>
+                </form>
             </div>
         </div>
         )
--- src/pages/AuthPages/register.jsx
+++ src/pages/AuthPages/register.jsx
@@ -1,38 +1,91 @@
     import { Link } from "react-router-dom";
-    import loginImg from "../../assets/logo-3.png"
+    import loginImg from "../../assets/logo-2.png"
     import "./register.css";
 
-    function RegisterPage(){
+
+    function loginPage(){
         return(
         <div className="registerMain">
-            <div className="userType">
-                <h1>Register as</h1>
-                <div className="btns1">
-                    <button>Customer</button>
-                    <button>Truck owner</button>
-                </div>
-            </div>
             <div className="register">
-                <form>
-                    <h1>Sign Up</h1>
-                        <input type="text" name="name" placeholder="Full Name" required />
-                        <input type="text" name="username" placeholder="Truck Name" />
-                        <label>Gender:
-                            <input type="radio" name="gender" /> Male
-                            <input type="radio" name="gender" /> Female
-                        </label>
-                        <input type="text" name="address" placeholder="Address" required/>
-                        <input type="tel" name="phone" placeholder="Phone" required/>
-                        <input type="password" name="password" placeholder="Password"required />
-                        <input type="password" name="password" placeholder="Password Confirmation"required />
-                    <div className="pt1">
-                        <Link to="/login">Already Has Account?</Link>
-                    </div>
-                    <button type="submit">Register</button>
-                </form>
+
                 <img src={loginImg} alt="Truckspot"/>
+                <h1>Create your account</h1>
+                <p>Join TruckSpot today</p>
+
+                <form>
+
+                    <label>Iam a...</label>
+                    <div className="type">
+                        
+                        <button className="customer">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+                            stroke-linecap="round" stroke-linejoin="round" 
+                            class="lucide lucide-user h-5 w-5"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle></svg>
+                            Customer
+                        </button>
+                        <button className="truck">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+                            stroke-linecap="round" stroke-linejoin="round" 
+                            class="lucide lucide-truck h-5 w-5">
+                            <path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2"></path>
+                            <path d="M15 18H9"></path><path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14"></path>
+                            <circle cx="17" cy="18" r="2"></circle><circle cx="7" cy="18" r="2"></circle></svg>
+                            Truck Owner
+                        </button>
+                        <button id="delivery" >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+                            stroke-linecap="round" stroke-linejoin="round" 
+                            class="lucide lucide-bike h-5 w-5"><circle cx="18.5" cy="17.5" r="3.5"></circle>
+                            <circle cx="5.5" cy="17.5" r="3.5"></circle><circle cx="15" cy="5" r="1"></circle>
+                            <path d="M12 17.5V14l-3-3 4-3 2 3h2"></path></svg>
+                            Delivery
+                        </button>
+                        
+                    </div>
+
+                    <label>Full Name</label>
+                    <div className="name">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                        viewBox="0 0 24 24" fill="none" stroke="#464343b2" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" 
+                        class="lucide lucide-user absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground">
+                        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle></svg>
+                        <input type="text" name="username" placeholder="Muhammed Ramadan" required/>
+                    </div> 
+
+                    <label>Email</label>
+                    <div className="email">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" 
+                        viewBox="0 0 24 24" fill="none" stroke="#464343b2" stroke-width="2" 
+                        stroke-linecap="round" stroke-linejoin="round" 
+                        class="lucide lucide-mail absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground">
+                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
+                        <input type="text" name="username" placeholder="you@example.com" required/>
+                    </div> 
+
+                    <label>Password</label>
+                    <div className="pass">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 24 24" fill="none" stroke="#464343b2" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" 
+                        class="lucide lucide-lock absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground">
+                        <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
+                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
+                        <input type="password" name="password" placeholder="••••••••" required />
+                    </div>
+
+                    <button className="registerBtn" type="submit">Create Account</button>
+
+                    <p>Already have an account? <Link to="/login">Sign In</Link></p>
+                </form>
             </div>
         </div>
         )
     }
-    export default RegisterPage;
+    export default loginPage;
