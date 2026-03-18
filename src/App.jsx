9023bbfc6471021b6d47d5e679cb787a902e4170
Muhammed Ramadan <muhammed.bussiness9@gmail.com>
2026-03-19 01:02:26 +0200

    Fix changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,15 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 // بتحكم هنا في ظهور الهيدر والفوتر علشان لو موجود في اللوجين و الرجيستر يختفي
 function Layout({ children }) {
   const location = useLocation();
-  const hideHeaderFooter = location.pathname === "/login" || location.pathname === "/register"; 
+  const hideLayout = location.pathname === "/login" || location.pathname === "/register";
   return (
     <>
-      {!hideHeaderFooter && <Header />}
-      <main>{children}
-      <AppSection />
-      </main>
-      {!hideHeaderFooter && <Footer />}
+      {!hideLayout && <Header />}
+        <main>
+          {children}
+          {!hideLayout && <AppSection />}
+        </main>
+      {!hideLayout && <Footer />}
     </>
   );
 }
